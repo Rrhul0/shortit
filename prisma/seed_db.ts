29daafa0c03421badd93148878a5d24bee367533
Rrhul0 <rr025765@gmail.com>
2022-11-17 17:21:17 +0530

    added seed script and required packages for it

---
+++ prisma/seed_db.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+import { faker } from '@faker-js/faker'
+
+const prisma = new PrismaClient()
+
+const MAX_NUMBER_OF_URLS = 10
+
+const data = Array.from({ length: MAX_NUMBER_OF_URLS }).map(() => ({
+    path: faker.word.noun(),
+    url: faker.internet.url(),
+}))
+
+async function main() {
+    for (let entry of data) {
+        await prisma.uRL.create({
+            data: {
+                path: entry.path,
+                to_url: entry.url,
+            },
+        })
+    }
+}
+
+main().finally(async () => {
+    await prisma.$disconnect()
+})
