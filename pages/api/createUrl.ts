d4f9142bf072c1c7fa93a89fd9700eab36b58b31
Rrhul0 <rr025765@gmail.com>
2022-11-19 11:05:01 +0530

    added prismaclient in lib to use everywhere and added create url api route

---
+++ pages/api/createUrl.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../lib/prisma'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'POST') {
+        res.status(405).end(req.method + ' method is not allowed')
+        return
+    }
+
+    const fullURL: string = JSON.parse(req.body).url
+    const url = await prisma.uRL.create({
+        data: {
+            to_url: fullURL,
+        },
+    })
+
+    res.status(201).json({ id: url.id })
+}
