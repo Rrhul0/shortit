72d9e933a0c1a0ed21ff9d3f94932ba167533100
Rrhul0 <rr025765@gmail.com>
2023-01-01 21:53:33 +0530

    fix input by removing outline

--- components/addPath.tsx
+++ components/addPath.tsx
@@ -41,16 +41,16 @@ export default function AddPath({
     }
 
     return (
-        <form onSubmit={onSubmitPath} className='flex border rounded-md'>
+        <form onSubmit={onSubmitPath} className='flex rounded-md '>
             <input
                 type='text'
                 value={pathValue}
-                className='rounded-l-md px-2 outline-white transition-all outline outline-2 hover:outline-blue-400 focus-visible:outline-blue-400 hover:outline hover:outline-offset-0 hover:outline-2 focus-visible:outline focus-visible:outline-2'
+                className='rounded-l-md px-2 bg-stone-100 focus:shadow-inner focus:outline-none'
                 onChange={e => setPathValue(e.currentTarget.value.replace(' ', '-'))}
             />
             <button
                 type='submit'
-                className='bg-emerald-500 outline-emerald-500 outline outline-2 px-2 rounded-r-md text-white hover:bg-emerald-600 hover:outline-emerald-600 transition-all'
+                className='bg-emerald-500 px-2 rounded-r-md text-white hover:bg-emerald-600 transition-all'
             >
                 Add
             </button>
