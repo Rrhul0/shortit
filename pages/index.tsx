499ae80f5025c46b95614f79729e5aafd4dabeea
Rrhul0 <rr025765@gmail.com>
2023-01-01 19:29:32 +0530

    change main title of home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <div className='flex flex-col items-center gap-6'>
             <h1 className='font-extrabold text-fuchsia-700 text-5xl mt-12 text-center drop-shadow-lg'>
-                Welcome to ShortIt
+                Create and Manage Short URLs
             </h1>
 
             <CreateUrlForm setProcessing={setProcessing} />
