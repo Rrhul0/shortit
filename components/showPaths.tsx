bf8be31fbae60d1cbf952ff680ecdf384d6c6a08
Rrhul0 <rr025765@gmail.com>
2023-01-11 14:22:46 +0530

    added next auth for authentication in app and prisma adapter for save user information

--- components/showPaths.tsx
+++ components/showPaths.tsx
@@ -12,11 +12,11 @@ const ShowPaths = ({ path }: { path?: string }) => {
         <li className='group relative'>
             {path ? (
                 <>
-                    <div className='bg-blue-300 bg-opacity-30 group-hover:blur-[1px] text-blue-700 transition-all px-2 py-1 rounded-xl'>
+                    <div className='bg-blue-300 bg-opacity-30 group-hover:blur-[1.5px] text-blue-700 transition-all px-2 py-1 rounded-xl'>
                         {path}
                     </div>
                     <CopyToClipboard text={path} onCopy={() => setCopied(true)}>
-                        <button className='absolute text-white inset-0 opacity-80 bg-blue-800 bg-opacity-60 z-10 hidden group-hover:block rounded-xl'>
+                        <button className='absolute text-white inset-0 bg-blue-800 transition-all bg-opacity-50 z-10 hidden group-hover:block rounded-xl'>
                             {copied ? 'Copied!' : 'Click to copy'}
                         </button>
                     </CopyToClipboard>
