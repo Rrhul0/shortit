ba6254f3fc88f918e8e14113ede2bf81a0bb24cf
Rrhul0 <rr025765@gmail.com>
2023-05-13 13:53:07 +0530

    added primary and background colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#E5E2E1',
+                bg: '#151517',
+            },
+        },
     },
     plugins: [],
 }
