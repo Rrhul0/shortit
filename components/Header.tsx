e45b20f3f81147f58c5cf2d31bb827d0d699090c
Rrhul0 <rr025765@gmail.com>
2023-05-13 13:53:41 +0530

    added header items

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,70 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Header() {
-    const { data, status } = useSession()
+    const { data } = useSession()
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
-        <header className='flex items-center justify-end mx-4'>
-            {status === 'unauthenticated' ? (
-                <button onClick={() => signIn()} className='underline'>
-                    SignIn
-                </button>
-            ) : (
-                <div className='h-10 w-10 rounded-full overflow-hidden grid place-items-center bg-gray-300'>
-                    {data?.user?.image ? (
-                        // eslint-disable-next-line @next/next/no-img-element
-                        <img width='40' height='40' src={data?.user?.image} alt={data.user.name || ''} />
-                    ) : (
-                        <div className='text-2xl font-semibold text-purple-600'>{data?.user?.name?.charAt(0)}</div>
-                    )}
-                </div>
-            )}
+        <header className='relative flex items-center justify-between px-8 bg-[#151517]'>
+            <h1 className='font-extrabold text-[#E5E2E1] text-4xl text-center drop-shadow-lg'>
+                ShortIt : <span className='font-bold text-2xl'>Create and Manage Short URLs</span>
+            </h1>
+            <div>
+                {!data?.user ? (
+                    <div className='flex gap-2'>
+                        <Link
+                            href='/api/auth/signin'
+                            onClick={() => signIn()}
+                            className='px-2 py-1 border border-transparent text-primary rounded-md transition-all hover:border-blue-600'
+                        >
+                            Log In
+                        </Link>
+                        <Link
+                            href='/api/auth/signin'
+                            onClick={() => signIn()}
+                            className='border px-2 py-1 rounded-md border-blue-600 text-primary transition-all hover:bg-blue-600'
+                        >
+                            Sign Up
+                        </Link>
+                    </div>
+                ) : (
+                    <button
+                        onClick={() => setShowMenu(o => !o)}
+                        className='h-10 w-10 rounded-full overflow-hidden grid place-items-center bg-gray-300'
+                    >
+                        {data.user.image ? (
+                            <span
+                                className='w-10 h-10 bg-white bg-cover bg-no-repeat'
+                                style={{ backgroundImage: `url('${data.user.image}')` }}
+                            ></span>
+                        ) : (
+                            <div className='text-2xl font-semibold text-purple-600'>{data?.user?.name?.charAt(0)}</div>
+                        )}
+                        <div
+                            className={`absolute w-80 h-24 z-50 backdrop-blur-sm  top-12 right-2 bg-slate-400 bg-opacity-50 rounded-xl p-2 gap-2 flex flex-col ${
+                                showMenu ? ' ' : 'hidden'
+                            }`}
+                        >
+                            <strong>{data.user.name}</strong>
+                            <small>{data.user.email}</small>
+                            <Link
+                                href='/api/auth/signout'
+                                onClick={e => {
+                                    e.preventDefault()
+                                    signOut()
+                                }}
+                                style={{ fontSize: '2rem' }}
+                                className='bg-purple-100 rounded-lg'
+                            >
+                                LogOut
+                            </Link>
+                        </div>
+                    </button>
+                )}
+            </div>
         </header>
     )
 }
