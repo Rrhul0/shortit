8c5be8d5998b97b896738484bc32563fca44f6bf
Rrhul0 <rr025765@gmail.com>
2023-05-15 13:09:59 +0530

    changed the logic to get urls of user from db

--- pages/api/get-urls.ts
+++ pages/api/get-urls.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from './auth/[...nextauth]'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
@@ -7,31 +9,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return
     }
 
-    const sessionToken = req.cookies['next-auth.session-token']
+    const session = await getServerSession(req, res, authOptions)
 
-    // const to_url: string = JSON.parse(req.body).url
+    if (session) {
+        const userId = session.user?.id
 
-    //getuserId
-    // let session: Session | null
-    if (!sessionToken) {
-        res.status(403).send('not logged in')
-        return
-    }
-    // else
-    const session = await prisma.session.findUnique({
-        where: { sessionToken },
-        include: { user: {} },
-    })
-    if (!session) {
-        res.status(403).send('not logged in')
-        return
-    }
-    const userId = session.userId
+        const url = await prisma.url.findMany({
+            where: { userId },
+            include: { paths: {} },
+        })
 
-    const url = await prisma.url.findMany({
-        where: { userId },
-        include: { paths: {} },
-    })
-
-    res.status(200).json(url)
+        res.status(200).json(url)
+    } else res.status(403).send('not logged in')
 }
