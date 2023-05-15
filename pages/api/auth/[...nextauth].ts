4c6529e37d131b3c339143125b256191f559143c
Rrhul0 <rr025765@gmail.com>
2023-05-15 16:24:47 +0530

    added discord provider to login with discord

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,11 +4,16 @@ import NextAuth, { AuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from 'next-auth/providers/email'
+import DiscordProvider from 'next-auth/providers/discord'
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     // Configure one or more authentication providers
     providers: [
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM,
+        }),
         GithubProvider({
             clientId: process.env.GITHUB_ID || '',
             clientSecret: process.env.GITHUB_SECRET || '',
@@ -17,9 +22,9 @@ export const authOptions: AuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID || '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
         }),
-        EmailProvider({
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM,
+        DiscordProvider({
+            clientId: process.env.DISCORD_CLIENT_ID || '',
+            clientSecret: process.env.DISCORD_CLIENT_SECRET || '',
         }),
 
         // ...add more providers here
